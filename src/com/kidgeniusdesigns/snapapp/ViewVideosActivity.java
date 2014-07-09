45f8d7caac80a59edae7119c3cacde47ceffced6
ben <bharv14@gmail.com>
2014-07-09 15:57:16 -0700

    Added video download

--- src/com/kidgeniusdesigns/snapapp/ViewVideosActivity.java
+++ src/com/kidgeniusdesigns/snapapp/ViewVideosActivity.java
@@ -21,6 +21,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,6 +31,8 @@ import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
@@ -48,20 +51,23 @@ public class ViewVideosActivity extends Activity {
 	ListView lv;
 
 	int vidCounter;
+	ProgressBar feedProgressBar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view_videos);
-vidCounter=0;
+		vidCounter = 0;
 		((MyApplication) getApplication())
 				.getTracker(MyApplication.TrackerName.APP_TRACKER);
 		ActionBar bar = getActionBar();
 		bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#517fa4")));
-		bar.setTitle("InstaSnap Story Videos");
+		bar.setTitle("InstaSnap");
 		bar.setIcon(new ColorDrawable(getResources().getColor(
 				android.R.color.transparent)));
 
+		feedProgressBar = (ProgressBar) findViewById(R.id.progressBar1);
+		feedProgressBar.setVisibility(ProgressBar.VISIBLE);
 		vidSenders = new ArrayList<String>();
 
 		for (Story s : SnapData.videoStorys) {
@@ -79,22 +85,27 @@ vidCounter=0;
 
 				try {
 
-				File tempVidFile = new File(getFilesDir() + "/video.zip");
+					File tempVidFile = new File(getFilesDir() + "/video.zip");
 					FileOutputStream out = new FileOutputStream(tempVidFile);
 					out.write(SnapData.videoByteList.get(position));
 					out.close();
 
-					File destinationFile= new File(getFilesDir() + "/videos");
+					File destinationFile = new File(getFilesDir() + "/videos");
+					unzip(tempVidFile.getAbsolutePath(),
+							destinationFile.getAbsolutePath());
 
-//unzip(tempVidFile.getAbsolutePath(),destinationFile.getAbsolutePath());
-					
 				} catch (IOException e) {
 					e.printStackTrace();
+					Toast.makeText(getApplicationContext(),
+							"This snap didn't load yet", Toast.LENGTH_LONG)
+							.show();
 				}
 			}
 
 		});
-
+		adapter = new ArrayAdapter<String>(getApplicationContext(),
+				R.layout.list_item, vidSenders);
+		lv.setAdapter(adapter);
 		LoadStories ls = new LoadStories();
 		ls.execute();
 
@@ -201,40 +212,52 @@ vidCounter=0;
 
 		@Override
 		protected void onPostExecute(String result) {
+			adapter.notifyDataSetChanged();
+			lv.invalidateViews();
 
-			adapter = new ArrayAdapter<String>(getApplicationContext(),
-					R.layout.list_item, vidSenders);
-			lv.setAdapter(adapter);
 			System.out.println("Done");
+			if (checkEverySnapIndex < vidSenders.size() - 5) {
+				LoadStories ls = new LoadStories();
+				ls.execute();
+			} else {
+				feedProgressBar.setVisibility(ProgressBar.GONE);
+			}
 
 		}
 	}
 
 	public void addToGallery(File f) {
-		// Save the name and description of a video in a ContentValues map.  
-        ContentValues values = new ContentValues(2);
-        values.put(MediaStore.Video.Media.MIME_TYPE, "video/mp4");
-         values.put(MediaStore.Video.Media.DATA, f.getAbsolutePath()); 
+		File direct = new File(Environment.getExternalStorageDirectory(),
+				"Saved Snaps");
+		// Save the name and description of a video in a ContentValues map.
+		ContentValues values = new ContentValues(2);
+		values.put(MediaStore.Video.Media.MIME_TYPE, "video/mp4");
+		values.put(MediaStore.Video.Media.DATA, direct.getPath() + vidCounter
+				+ ".mp4");
 
-        // Add a new record (identified by uri) without the video, but with the values just set.
-        Uri uri = getContentResolver().insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values);
+		// Add a new record (identified by uri) without the video, but with the
+		// values just set.
+		Uri uri = getContentResolver().insert(
+				MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values);
 
-        // Now get a handle to the file for that record, and save the data into it.
-        try {
-            InputStream is = new FileInputStream(f);
-            OutputStream os = getContentResolver().openOutputStream(uri);
-            byte[] buffer = new byte[4096]; // tweaking this number may increase performance
-            int len;
-            while ((len = is.read(buffer)) != -1){
-                os.write(buffer, 0, len);
-            }
-            os.flush();
-            is.close();
-            os.close();
-        } catch (Exception e) {
-        } 
+		// Now get a handle to the file for that record, and save the data into
+		// it.
+		try {
+			InputStream is = new FileInputStream(f);
+			OutputStream os = getContentResolver().openOutputStream(uri);
+			byte[] buffer = new byte[4096]; // tweaking this number may increase
+											// performance
+			int len;
+			while ((len = is.read(buffer)) != -1) {
+				os.write(buffer, 0, len);
+			}
+			os.flush();
+			is.close();
+			os.close();
+		} catch (Exception e) {
+		}
 
-        sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri));
+		sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri));
 	}
 
 	@Override
@@ -267,47 +290,50 @@ vidCounter=0;
 	public void back(View v) {
 		finish();
 	}
-	
-	public void unzip(String zipFilePath, String destDirectory) throws IOException {
-        File destDir = new File(destDirectory);
-        if (!destDir.exists()) {
-            destDir.mkdir();
-        }
-        ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath));
-        ZipEntry entry = zipIn.getNextEntry();
-        // iterates over entries in the zip file
-        while (entry != null) {
-            String filePath = destDirectory + File.separator + vidCounter+".mp4";
-            vidCounter++;
-            if (!entry.isDirectory()) {
-                // if the entry is a file, extracts it
-                extractFile(zipIn, filePath);
-                
-            } else {
-                // if the entry is a directory, make the directory
-                File dir = new File(filePath);
-                dir.mkdir();
-            }
-            zipIn.closeEntry();
-            entry = zipIn.getNextEntry();
-        }
-        zipIn.close();
-    }
-	private void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
-        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
-        byte[] bytesIn = new byte[BUFFER_SIZE];
-        int read = 0;
-        while ((read = zipIn.read(bytesIn)) != -1) {
-            bos.write(bytesIn, 0, read);
-        }
-        bos.close();
-    
-        System.out.println(filePath);
-        //addToGallery(new File(filePath));
-        if((vidCounter%2)==1){
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(filePath));
-        intent.setDataAndType(Uri.parse(filePath), "video/mp4");
-        startActivity(intent);
-        }
-    }
+
+	public void unzip(String zipFilePath, String destDirectory)
+			throws IOException {
+		File destDir = new File(destDirectory);
+		if (!destDir.exists()) {
+			destDir.mkdir();
+		}
+		ZipInputStream zipIn = new ZipInputStream(new FileInputStream(
+				zipFilePath));
+		ZipEntry entry = zipIn.getNextEntry();
+		// iterates over entries in the zip file
+		while (entry != null) {
+			String filePath = destDirectory + File.separator + vidCounter
+					+ ".mp4";
+			vidCounter++;
+			if (!entry.isDirectory()) {
+				// if the entry is a file, extracts it
+				if (entry.getSize() > 750000)
+					extractFile(zipIn, filePath);
+			} else {
+				// if the entry is a directory, make the directory
+				File dir = new File(filePath);
+				dir.mkdir();
+			}
+			zipIn.closeEntry();
+			entry = zipIn.getNextEntry();
+		}
+		zipIn.close();
+	}
+
+	private void extractFile(ZipInputStream zipIn, String filePath)
+			throws IOException {
+		BufferedOutputStream bos = new BufferedOutputStream(
+				new FileOutputStream(filePath));
+		byte[] bytesIn = new byte[BUFFER_SIZE];
+		int read = 0;
+		while ((read = zipIn.read(bytesIn)) != -1) {
+			bos.write(bytesIn, 0, read);
+		}
+		bos.close();
+
+		System.out.println(filePath);
+		addToGallery(new File(filePath));
+		Toast.makeText(getApplicationContext(), "Saved to Gallery. folder 0",
+				Toast.LENGTH_LONG).show();
+	}
 }
